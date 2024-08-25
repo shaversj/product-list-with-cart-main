5344a0b969fd6a7f49c0f8747a76c2b3ae23e576
Josh Shavers <shaversj@gmail.com>
2024-08-25 09:05:32 -0500

    Fix mobile and tablet layout

--- src/components/cart/EmptyCartMessage.tsx
+++ src/components/cart/EmptyCartMessage.tsx
@@ -1,6 +1,6 @@
 export default function EmptyCartMessage() {
   return (
-    <div className={"flex flex-col items-center justify-center pt-[2.5rem]"}>
+    <div className={"flex flex-col items-center justify-center pb-10 pt-[2.5rem]"}>
       <img src={"/assets/images/illustration-empty-cart.svg"} alt={"Empty Cart"} className={"h-[8rem] w-[8rem]"} />
       <span className={"inline-block pt-4 text-[0.875rem] font-semibold text-rose-500"}>Your added items will appear here</span>
     </div>
