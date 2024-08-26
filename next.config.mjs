63c53acc6d90347cacd32c82dd2b0ccaffa267c2
Josh Shavers <shaversj@gmail.com>
2024-08-26 07:28:26 -0500

    Enable Github Action and Pages

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  output: "export",
+  basePath: "/product-list-with-cart-main",
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
